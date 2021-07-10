4fa5040392a1d98d372e4e9d29eb903b2b6da7fc
lisaacedo <87204470+lisaacedo@users.noreply.github.com>
2021-07-11 01:06:43 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+##
+	## For matrix let it be represented by x
+	## then let "o" be for the null solved value
+	## the "mean" would then be replaced with "solve" 
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix(sample(1:400,7),5,5)) {
+o <- NULL
+set <- function(y) {
+x <<- y
+o <<- NULL
+}
+get <- function() x
+     setsolve <- function(solve) o <<- solve
+     getsolve <- function() o
+     list(set = set, get = get,
+	       setsolve = setsolve,
+	       getsolve = getsolve)
+        
 }
 
-
-## Write a short comment describing this function
-
+## as for this part, "mean" was also replaced with "solve" and with "m" to "o"
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+o <- x$getsolve()
+if(!is.null(o)) {
+    message("getting inversed matrix")
+    return(o)
 }
+    data <- x$get()
+    o <- solve(data, ...)
+    x$setsolve(o)
+    o
+}
+
